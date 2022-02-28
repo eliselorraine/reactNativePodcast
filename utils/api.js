a9db58a5d2facf9a630ec808b611beb5e4f08030
Elise Lawrence <eliselorraine@icloud.com>
2022-02-28 15:15:07 +0100

    api call function

--- utils/api.js
+++ utils/api.js
@@ -1,8 +1,26 @@
-import { API_URL, API_TOKEN } from '@env';
+// import { LISTEN_API_URL, LISTEN_API_KEY } from '@env';
+const { Client } = require('podcast-api');
 
-export const apiCall = async (q) => {
-    console.log('here', API_URL);
-    console.log(q);
-   
-}
+const client = Client({ apiKey: LISTEN_API_KEY });
+
+client.search({
+    q: 'star wars',
+}).then((response) => {
+    console.log(response.data)
+}).catch(e => console.log(e.message));
+
+// export const apiCall = (q) => {
+//     // console.log(LISTEN_API_URL, LISTEN_API_KEY);
+//     console.log(q);
+//     fetch(`${LISTEN_API_URL}/search?q=${q}`, {
+//         method: 'GET',
+//         headers: {
+//             'X-ListenAPI-Key': `${LISTEN_API_KEY}`,
+//         },
+
+//     }).then(data => console.log(data.json()))
+//         .catch((error) => console.log(error.message))
+
+
+// }
 
