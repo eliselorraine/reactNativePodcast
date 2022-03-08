33b996ba6ab7d889240cf36d46b4ce0dcbd6f253
Elise Lawrence <eliselorraine@icloud.com>
2022-03-08 10:07:35 +0100

    test audio working with pause

--- components/Podcast.js
+++ components/Podcast.js
@@ -3,10 +3,40 @@ import { useEffect, useState } from 'react';
 import { apiCall } from '../utils/api';
 import HTML from 'react-native-render-html';
 // import TrackPlayer, { RepeatMode } from 'react-native-track-player';
-// import { Audio } from 'expo-av';
+import { Audio } from 'expo-av';
 
 
 export default Podcast = ({ title, thumbnail, description }) => {
+    const [sound, setSound] = useState(new Audio.Sound());
+    const setup = async () => {
+        Audio.setAudioModeAsync({
+            allowsRecordingIOS: false,
+            interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+            playsInSilentModeIOS: true,
+            interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+            shouldDuckAndroid: true,
+            staysActiveInBackground: true,
+            playsThroughEarpieceAndroid: true,
+        })
+
+        const status = {
+            shouldPlay: false,
+        }
+
+        sound.loadAsync(require('../assets/test.mp3'), status, false);
+    }
+    
+    useEffect(() => {
+        setup()
+    }, [])
+
+    const playSound = () => {
+        sound.playAsync();
+    }
+    
+    const pauseSound = () => {
+        sound.pauseAsync();
+    }
 
     const { width } = useWindowDimensions();
     return (
@@ -29,9 +59,12 @@ export default Podcast = ({ title, thumbnail, description }) => {
                     source={{ uri: thumbnail }}
                 />
             </View>
-            {/* <View>
+            <View>
                 <Button title='play sound' onPress={playSound} />
-            </View> */}
+            </View>
+            <View>
+                <Button title='pause sound' onPress={pauseSound} />
+            </View>
             <HTML
                 contentWidth={width}
                 source={{ html: `<p>${description}</p>` }}
