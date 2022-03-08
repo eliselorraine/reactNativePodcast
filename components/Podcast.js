2f6d4ea81e762b2827058194b7aaca6f132b92d7
Elise Lawrence <eliselorraine@icloud.com>
2022-03-08 10:47:47 +0100

    loading audio from API

--- components/Podcast.js
+++ components/Podcast.js
@@ -1,13 +1,12 @@
 import { Image, StyleSheet, View, Text, Button, useWindowDimensions } from 'react-native';
 import { useEffect, useState } from 'react';
-import { apiCall } from '../utils/api';
 import HTML from 'react-native-render-html';
-// import TrackPlayer, { RepeatMode } from 'react-native-track-player';
 import { Audio } from 'expo-av';
 
 
-export default Podcast = ({ title, thumbnail, description }) => {
+export default Podcast = ({ title, thumbnail, description, audio }) => {
     const [sound, setSound] = useState(new Audio.Sound());
+
     const setup = async () => {
         Audio.setAudioModeAsync({
             allowsRecordingIOS: false,
@@ -18,24 +17,35 @@ export default Podcast = ({ title, thumbnail, description }) => {
             staysActiveInBackground: true,
             playsThroughEarpieceAndroid: true,
         })
-
-        const status = {
-            shouldPlay: false,
-        }
-
-        sound.loadAsync(require('../assets/test.mp3'), status, false);
     }
     
     useEffect(() => {
         setup()
     }, [])
 
-    const playSound = () => {
-        sound.playAsync();
+    const playSound = async () => {
+        const status = {
+            shouldPlay: false,
+        }
+
+        const source = {
+            uri: audio,
+            overrideFileExtensionAndroid: '.mp3',
+        }
+
+        const loadStatus = await sound.getStatusAsync();
+        
+        if (loadStatus.isLoaded) {
+            await sound.playAsync();
+            return;
+        }
+    
+        await sound.loadAsync(source, status, false);
+        await sound.playAsync();
     }
     
-    const pauseSound = () => {
-        sound.pauseAsync();
+    const pauseSound = async () => {
+        await sound.pauseAsync();
     }
 
     const { width } = useWindowDimensions();
@@ -60,7 +70,10 @@ export default Podcast = ({ title, thumbnail, description }) => {
                 />
             </View>
             <View>
-                <Button title='play sound' onPress={playSound} />
+                <Button 
+                    title='play sound' 
+                    onPress={playSound}
+                />
             </View>
             <View>
                 <Button title='pause sound' onPress={pauseSound} />
